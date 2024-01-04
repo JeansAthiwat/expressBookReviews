9a0d312222825767a5514a89aa1fe916ca1e2c67
JeansAthiwat <athiwat5146@gmail.com>
2024-01-04 14:45:30 +0700

    to be imp: register

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,6 +7,19 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
+  ```  const username = req.params.username;
+  const password = req.params.password;
+
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({ "username": username, "password": password });
+      return res.status(200).json({ message: "User successfully registred. Now you can login" });
+    } else {
+      return res.status(404).json({ message: "User already exists!" });
+    }
+  }
+
+  return res.status(404).json({ message: "Unable to register user." });```
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
